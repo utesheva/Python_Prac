ac789e633882fa048b7a07ae5c9430dbdacebda8
Ekaterina <utesheva.e.e@gmail.com>
2024-10-15 11:12:11 +0300

    timeit + set

--- 20241015/0/prog1.py
+++ 20241015/0/prog1.py
@@ -1,6 +1,12 @@
 import string
-g = {'a', 'e', 'u', 'y', 'o', 'i'}
-s = set(string.ascii_lowercase) - g
-st = set(input())
-print(len(st & g))
-print(len(st & s))
+import timeit
+
+def wov_cons(st):
+    g = {'a', 'e', 'u', 'y', 'o', 'i'}
+    s = set(string.ascii_lowercase) - g
+    st = set(st)
+    return len(st & g), len(st & s)
+
+st = input()
+T = timeit.Timer("wov_cons", globals = globals())
+print(T.autorange())
