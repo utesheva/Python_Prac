3106128eed931fd35d1938a82c03f4be27a05bfc
Ekaterina <utesheva.e.e@gmail.com>
2024-10-31 11:44:45 +0300

    hw

--- 20241022/2/prog.py
+++ 20241022/2/prog.py
@@ -1,1 +1,5 @@
-def slide(seq, n)
+from itertools import islice, tee
+def slide(seq, n):
+    yield from (i for st in range(len(seq)) for i in islice(seq, st, st + n))
+import sys
+exec(sys.stdin.read())
