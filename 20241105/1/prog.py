a044ea53cce646055f2d46e2a087e05b642131f8
Ekaterina <utesheva.e.e@gmail.com>
2024-11-11 17:32:52 +0300

    added Omnibus;

--- 20241105/1/prog.py
+++ 20241105/1/prog.py
@@ -1,26 +1,19 @@
 class Omnibus:
+    exempl = {}
     def __setattr__(self, attr, n):
-        if attr in dir(Omnibus):
-            setattr(Omnibus, attr, getattr(Omnibus, attr) + 1)
+        if attr in Omnibus.exempl:
+            Omnibus.exempl[attr].add(self)
         else:
-            setattr(Omnibus, attr, 1)
+            Omnibus.exempl[attr] = set([self])
 
     def __getattribute__(self, attr):
-        return getattr(Omnibus, attr)
+        if attr in Omnibus.exempl:
+            return len(Omnibus.exempl[attr])
+        return 0
 
     def __delattr__(self, attr):
-        if attr in dir(Omnibus):
-            setattr(Omnibus, attr, getattr(Omnibus, attr) - 1)
+        if attr in Omnibus.exempl:
+            Omnibus.exempl[attr].discard(self)
 
-a, b, c = Omnibus(), Omnibus(), Omnibus()
-del a.random
-a.i = a.j = a.k = True
-b.j = b.k = b.n = False
-c.k = c.n = c.m = hex
-print(a.i, a.j, a.k, b.j, b.k, b.n, c.k, c.n, c.m)
-del a.k, b.n, c.m
-print(a.i, a.j, b.j, b.k, c.k, c.n)
-del a.k, c.m
-print(a.i, a.j, b.j, b.k, c.k, c.n)
-a.k = b.i = c.m = 777
-print(a.i, a.j, a.k, b.j, b.k, c.k, c.n, c.m)
+import sys
+exec(sys.stdin.read())
